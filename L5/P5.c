fbd17185161c59a992b5684c70b017ed108caf73
Cotrau-Catalin <61750604+Cotrau-Catalin@users.noreply.github.com>
2020-03-29 16:22:31 +0300

    Update P5.c

--- L5/P5.c
+++ L5/P5.c
@@ -3,47 +3,47 @@
 //adauga într-o a doua stiva sub forma binara.
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 50
-int stack1[MAX_SIZE], stack2[MAX_SIZE], top1=-1, top2=-1;
+#define SIZE 50
+int stack1[SIZE], stack2[SIZE], t1=-1, t2=-1;
 void push1(int nr)
 {
-	if (top1 == MAX_SIZE - 1)
+	if (t1 == SIZE - 1)
 	{
 		printf("\nEroare:Stiva este plina\n ");
 		return;
 	}
-	stack1[++top1] = nr;
+	stac1[++t1] = nr;
 }
-void pop1()
+void po1()
 {
-	if (top1 == -1)
+	if (t1 == -1)
 	{
 		printf("\nEroare:stiva este goala\n");
 		return;
 	}
-	top1--;
+	t1--;
 }
-void push2(int nr)
+void pus2(int nr)
 {
-	if (top2 == MAX_SIZE - 1)
+	if (t2 == SIZE - 1)
 	{
 		printf("\nEroare:Stiva este plina\n ");
 		return;
 	}
-	stack2[++top2] = nr;
+	stac2[++t2] = nr;
 }
-void pop2()
+void po2()
 {
-	if (top2 == -1)
+	if (t2 == -1)
 	{
 		printf("\nEroare:stiva este goala\n");
 		return;
 	}
-	top2--;
+	t2--;
 }
 int binar(int nr)
 {
-	int k = 0, binary[10],i,baza2=0;
+	int k = 0, binary[10],i,baze2=0;
 	while (nr > 0)
 	{
 		binary[k] = nr % 2;
@@ -51,8 +51,8 @@ int binar(int nr)
 		k++;
 	}
 	for (i = k - 1; i >= 0; i--)
-		baza2 = baza2 * 10 + binary[i];
-	return baza2;
+		baze2 = baze2 * 10 + binary[i];
+	return baza\e2;
 }
 
 int prim(int nr)
@@ -69,22 +69,22 @@ int main()
 {
 	int i,aux[50],k=0;
 	for (i = 1; i <= 50; i++)
-		push1(i);
-	while (top1 != -1)
+		pus1(i);
+	while (t1 != -1)
 	{
-		if (prim(stack1[top1]) == 1)
-			push2(binar(stack1[top1]));
+		if (prim(stac1[t1]) == 1)
+			pus2(binar(stac1[t1]));
 		else
-			aux[k++] = stack1[top1];
-		pop1();
+			aux[k++] = stac1[t1];
+		po1();
 	}
 	for (i = k - 1; i >= 0; i--)
-		push1(aux[i]);
-	for (i = 0; i <= top1; i++)
-		printf("%d ", stack1[i]);
+		pus1(aux[i]);
+	for (i = 0; i <= t1; i++)
+		printf("%d ", stac1[i]);
 	printf("\n");
-	for (i = 0; i <= top2; i++)
-		printf("%d ", stack2[i]);
+	for (i = 0; i <= t2; i++)
+		printf("%d ", stac2[i]);
 	printf("\n");
 	system("pause");
 	return 0;
